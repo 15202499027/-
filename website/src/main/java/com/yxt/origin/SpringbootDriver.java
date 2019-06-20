a2751a80857fbb8b7c2cd2b3685d16293861777d
yxt <yxt_hp@163.com>
2019-06-20 15:32:52 +0800

    2019年6月20日

---
+++ website/src/main/java/com/yxt/origin/SpringbootDriver.java
@@ -0,0 +1,12 @@
+package com.yxt.origin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDriver {
+    public static void main( String[] args ){
+        SpringApplication.run(SpringbootDriver.class,args);
+    }
+}
